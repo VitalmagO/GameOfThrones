197ec3ec8d90b8e3a671aaab2252351d6df635e7
vitalmobile <melnichenkova@mail.skblab.ru>
2022-03-14 10:56:07 +0600

    Edit Utils.kt, add check network

--- app/src/main/java/ru/skillbranch/gameofthrones/Utils.kt
+++ app/src/main/java/ru/skillbranch/gameofthrones/Utils.kt
@@ -1,5 +1,10 @@
 package ru.skillbranch.gameofthrones
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.NetworkInfo
+import android.os.Build
 import android.util.Log
 import io.reactivex.Observable
 import io.reactivex.Single
@@ -21,6 +26,31 @@ fun createRequest(url: String) = Observable.create<String> {
     } finally {
         urlConnection.disconnect()
     }
+}
+
+fun isNetworkAvailable(context: Context): Boolean {
+    val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    // For 29 api or above
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+                ?: return false
+        return when {
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            else -> false
+        }
+    }
+    // For below 29 api
+    else {
+        if (connectivityManager.activeNetworkInfo != null && connectivityManager.activeNetworkInfo!!.isConnectedOrConnecting) {
+            return true
+        }
+    }
+    return false
 }
 
 fun <T> Observable<T>.async(): Observable<T> = subscribeOn(
